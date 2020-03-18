1d9c82bd43d7ad652d0b8390c708b051b8d3f0fa
Terry Threatt <terry.threatt@gmail.com>
2020-03-18 14:07:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
+# flatiron html 
 html = open("https://flatironschool.com/")
+
+# coverting strings of html to node 
+doc = Nokogiri::HTML(html)
+puts doc.css(".headline-26OIBN").text.strip 
+
+
+
